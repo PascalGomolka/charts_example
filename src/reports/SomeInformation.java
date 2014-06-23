9288c71072a016047321a8d1faee48bf65a13c37
Passe <pascal@gomolka-online.com>
2014-06-23 21:10:06 +0200

    initial commit with example data

---
+++ src/reports/SomeInformation.java
@@ -0,0 +1,19 @@
+package reports;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SomeInformation {
+
+    public static List getAverages(){
+        List<Integer> avg = new ArrayList<Integer>();
+
+        avg.add(10);
+        avg.add(75);
+        avg.add(31);
+        avg.add(89);
+        avg.add(29);
+
+        return avg;
+    }
+}
